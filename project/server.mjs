c55a3c722620c62c1cfef8867b2415aad6f93ea4
maxime.dubourg <maxime.dubourg@ekino.com>
2025-03-19 17:13:58 +0100

    setup for step 2

--- project/server.mjs
+++ project/server.mjs
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 nunjucks.configure("views", {
   autoescape: true,
   express: app,
+  noCache: true,
 });
 
 // ----- routes -----
@@ -33,6 +34,34 @@ app.post("/todos", async (req, res) => {
   res.render("components/molecules/todolist.njk", {
     todos,
   });
+});
+
+// Toggle todo
+
+app.post("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const todos = await toggleTodo(id);
+
+    res.render("components/molecules/todolist.njk", { todos });
+  } catch (error) {
+    res.status(404).send();
+  }
+});
+
+// Delete todo
+
+app.delete("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await deleteTodo(id);
+
+    res.status(200).send();
+  } catch (error) {
+    res.status(404).send();
+  }
 });
 
 app.listen(port, () => {
