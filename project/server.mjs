b590ebfb656ff52e01e1ea64e8e0fa6eab94c595
maxime.dubourg <maxime.dubourg@ekino.com>
2025-03-19 17:38:37 +0100

    setup for step 3

--- project/server.mjs
+++ project/server.mjs
@@ -15,7 +15,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 nunjucks.configure("views", {
   autoescape: true,
   express: app,
+  noCache: true,
 });
+
+// ----- subscribers -----
+
+const clients = new Set();
+
+function notifyClients(todos) {
+  const renderedTodos = nunjucks
+    .render("components/molecules/todolist.njk", { todos })
+    .replace(/(\r\n|\n|\r)/gm, "");
+
+  for (const client of clients) {
+    client.write(`data: ${renderedTodos}\n\n`);
+  }
+}
 
 // ----- routes -----
 
@@ -30,8 +45,53 @@ app.get("/", async (req, res) => {
 app.post("/todos", async (req, res) => {
   const todos = await addTodo(req.body.title);
 
-  res.render("components/molecules/todolist.njk", {
-    todos,
+  notifyClients(todos);
+
+  res.status(201).send();
+});
+
+// Toggle todo
+app.post("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const todos = await toggleTodo(id);
+
+    notifyClients(todos);
+    res.status(204).send();
+  } catch (error) {
+    res.status(404).send();
+  }
+});
+
+// Delete todo
+app.delete("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const todos = await deleteTodo(id);
+    notifyClients(todos);
+
+    res.status(204).send();
+  } catch (error) {
+    res.status(404).send();
+  }
+});
+
+// Server-sent events
+app.get("/live", (req, res) => {
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Connection", "keep-alive");
+  res.flushHeaders();
+
+  clients.add(res);
+
+  res.on("close", () => {
+    console.log("Client closed connection.");
+
+    clients.delete(res);
+    res.end();
   });
 });
 
